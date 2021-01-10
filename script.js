d25fab4795825055de6051b3c67d63e0124c69fe
Michael Humpherys <michaelhumpherys@Michaels-MacBook-Air.local>
2021-01-10 12:00:55 -0500

    random generated functions written

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 // Assignment code here
+var generatePassword = function() {
+  //lower
+  function randomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  }
+//upper
+  function randomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+  }
+//number
+function randomNumber() {
+return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+//symbol
+function randomSymbol() {
+var symbols = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+return symbols[Math.floor(Math.random() * symbols.length)];
+}
 
+  
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
