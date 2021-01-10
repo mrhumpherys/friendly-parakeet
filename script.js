07327bfd0c5f9335770ddff03c6c6f7bc3b94e9e
Michael Humpherys <michaelhumpherys@Michaels-MacBook-Air.local>
2021-01-10 12:35:48 -0500

    created window prompts

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 // Assignment code here
-var generatePassword = function() {
+//object with random functions
+var randomFunctions = {
+  lower: randomLower,
+  upper: randomUpper,
+  number: randomNumber,
+  symbol: randomSymbol
+}
+
+
   //lower
   function randomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
@@ -17,9 +25,29 @@ function randomSymbol() {
 var symbols = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 return symbols[Math.floor(Math.random() * symbols.length)];
 }
+var generatePassword = function() {
+  //determine lenght
+  var length = +window.prompt("Choose a password length anywhere betweeen 8 and 128");
+  if (length < 8 || length > 128) {
+    alert("You choose an incorrect value. Try again");
+    return generatePassword();
+  }
+  //determine password contents
+  var isLower = window.confirm("Would you like lowercase letters in your password?");
 
-  
+  var isUpper = window.confirm("Would you like uppercase letters in your password?");
+
+  var isNumber = window.confirm("Would you like numbers in your password?");
+
+  var isSymbol = window.confirm("Would you like symbols in your password?");
+
+//function with loop and ability to choose password makeup
+
+
+  console.log(isNumber);
 }
+  
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
